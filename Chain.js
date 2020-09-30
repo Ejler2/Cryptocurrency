bbb4e228efacf43d295de015739f06f3e319c461
Noah <noahejler@gmail.com>
2020-09-30 13:19:14 +0200

    Log opdate

---
+++ Chain.js
@@ -0,0 +1,15 @@
+const SHA256 = require('crypto-js/sha256');
+
+class Block{
+    constructor(index, tid, data, tidligerehash = ''){
+        this.index = index;
+        this.tid = tid;
+        this.data = data;
+        this.tidligerehash = tidligerehash;
+        this.hash = '';
+    }
+
+    genererhash{
+        return SHA256(this.index, this.tid, this.data, JSON.stringify(this.data)).toString();
+    }
+}
--- Log.md
+++ Log.md
@@ -1,2 +1,5 @@
 d. 28 sep
 Frederik og jeg har undersøgt hvordan blockchain fungere. Derudover også fundet videoer på nettet, som beskriver Cryptocurrency.
+
+d. 30 sep
+Der er blevet lavet lidt i forhold til en block class og der er blevet hentet et libery til Cryptocurrency.
